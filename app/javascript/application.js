022bd5b3e01fe21b2b6d270bb2ecd6f6efd22ee2
Viktoria <viktoriazlayaa@gmail.com>
2024-05-06 19:33:55 +0300

    Add alert messages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(function() {
+    document.querySelectorAll('.alert').forEach(function(alert) {
+      alert.style.display = 'none';
+    });
+  }, 3500);
+});
